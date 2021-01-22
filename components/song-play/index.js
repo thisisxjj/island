dcb5a85deed71a356e105fe85a4e5df4f7ffc87f
xiajj <763801840@qq.com>
2021-01-22 21:45:39 +0800

    1.解决无法跳过没有版权的歌曲的bug 2.添加播放音乐动画 提交人：xjj

--- components/song-play/index.js
+++ components/song-play/index.js
@@ -21,7 +21,10 @@ Component({
     playing: false,
     playSrc: './icons/playing-play.png',
     pauseSrc: './icons/playing-pause.png',
-    showPlayIcon: true
+    showPlayIcon: true,
+    prevSong: null,
+    nextSong: null,
+    checkedSongs: []
   },
   attached() {
     this.setData({
@@ -58,6 +61,117 @@ Component({
         playing: true
       })
     },
+    showDontPlayModal() {
+      wx.showModal({
+        content: '该歌曲请前往云音乐APP播放',
+        confirmText: '我知道了',
+        showCancel: false
+      })
+      this.setData({
+        showPlayIcon: false
+      })
+      app.playingSongId = 0
+    },
+    checkAndRequestSong(song) {
+      this.checkSong(song).then(res => {
+        if(res.success) {
+            return songDetailModel.getSongPlayUrl(song.id)
+        } else {
+          bam.stop()
+          return this.showDontPlayModal()
+        }
+      }).then(res => {
+        if(!res) {
+          return
+        }
+        this.setBackgroundAudioManager(song, res)
+      })
+    },
+    setBackgroundAudioManager(song, res) {
+      bam.title = song.name
+      bam.src = res.data[0].url
+      bam.singer = this._splice(song.ar) + ' - ' + song.al.name 
+      bam.coverImgUrl = song.al.picUrl
+      //设置全局的播放歌曲的songid
+      app.playingSongId = song.id
+      this.setData({
+        song,
+        playing: true
+      })
+    },
+    checkSong(song) {
+      return songDetailModel.checkSongUrl(song.id)
+    },
+    getSongIndexFromList(song, playlist) {
+      if(!playlist) {
+        return -1
+      }
+      let trackArray = playlist.tracks
+      for(let i = 0; i < trackArray.length; i++) {
+        if(song.id === trackArray[i].id) {
+          return i;
+        }
+      }
+      return -1
+    },
+    getPrevSong(index, playlist) {
+      let preIndex = index - 1 < 0 ? (playlist.tracks.length - 1) : (index - 1)
+      return playlist.tracks[preIndex]
+    },
+    getNextSong(index, playlist) {
+      let nextIndex = index + 1 >= playlist.tracks.length ? 0 : (index + 1)
+      return playlist.tracks[nextIndex]
+    },
+    setPlayPrev(song, playlist) {
+      let index = this.getSongIndexFromList(song, playlist)
+      let tempPrevSong = this.getPrevSong(index, playlist)
+      this.checkSong(tempPrevSong)
+        .then(res => {
+          if(res.success) {
+            this.setData({
+              prevSong: tempPrevSong
+            })
+            return
+          } else {
+            this.setPlayPrev(tempPrevSong, playlist)
+          }
+        })
+    },
+    setPlayNext(song, playlist) {
+      let index = this.getSongIndexFromList(song, playlist)
+      let tempNextSong = this.getNextSong(index, playlist)
+      this.checkSong(tempNextSong)
+        .then(res => {
+          if(res.success) {
+            this.setData({
+              nextSong: tempNextSong
+            })
+            return
+          } else {
+            this.setPlayNext(tempNextSong, playlist)
+          }
+        })
+    },
+    playNext() {
+      this.setData({
+        song: this.data.nextSong
+      })
+    },
+    playPrev() {
+      this.setData({
+        song: this.data.prevSong
+      })
+    },
+    _splice(arr) {
+      if(!arr) {
+        return
+      }
+      var temp = ''
+      for(var i = 0; i < arr.length; i++) {
+        temp += (i < arr.length - 1) ? (arr[i].name + '/') : arr[i].name 
+      }
+      return temp
+    },
     _monitorSwitch() {
       bam.onPlay(() => {
         this._recoverStatus()
@@ -78,101 +192,6 @@ Component({
       bam.onPrev(() => {
         this.playPrev()
       })
-    },
-    playPrev() {
-      if(!this.properties.playlist) {
-        return
-      }
-      let trackArray = this.properties.playlist.tracks
-      for(let i = trackArray.length - 1; i >= 0; i--) {
-        if(this.properties.song.id === trackArray[i].id) {
-          if(i >= 1) {
-            //播放上一首歌曲
-            this.checkAndRequestSong(trackArray[i - 1], true)
-              .then((res) => {
-                if(res) {
-                  this.playPrev()
-                }
-              })
-          } else {
-            this.checkAndRequestSong(trackArray[trackArray.length - 1])
-          }
-          break
-        }
-      }
-    },
-    playNext() {
-      if(!this.properties.playlist) {
-        return
-      }
-      let trackArray = this.properties.playlist.tracks
-      for(let i = 0; i < trackArray.length; i++) {
-        if(this.properties.song.id === trackArray[i].id) {
-          if(i < trackArray.length - 1) {
-            //播放下一首歌曲
-            this.checkAndRequestSong(trackArray[i + 1], true)
-              .then((res) => {
-                if(res) {
-                  this.playNext()
-                }
-              })
-          } else {
-            this.checkAndRequestSong(trackArray[0])
-          }
-          break
-        }
-      }
-    },
-    _splice(arr) {
-      if(!arr) {
-        return
-      }
-      var temp = ''
-      for(var i = 0; i < arr.length; i++) {
-        temp += (i < arr.length - 1) ? (arr[i].name + '/') : arr[i].name 
-      }
-      return temp
-    },
-    showDontPlayModal() {
-      wx.showModal({
-        content: '该歌曲请前往云音乐APP播放',
-        confirmText: '我知道了',
-        showCancel: false
-      })
-      this.setData({
-        showPlayIcon: false
-      })
-      app.playingSongId = 0
-    },
-    checkAndRequestSong(song, isNextOrPrev) {
-      songDetailModel.checkSongUrl(song.id).then(res => {
-        if(res.success) {
-          return songDetailModel.getSongPlayUrl(song.id)
-        } else {
-          if(!isNextOrPrev) {
-            bam.stop()
-            return this.showDontPlayModal()
-          } else {
-            return new Promise(resolve => {
-              resolve(true)
-            })
-          }
-        }
-      }).then(res => {
-        if(!res) {
-          return
-        }
-        bam.title = song.name
-        bam.src = res.data[0].url
-        bam.singer = this._splice(song.ar) + ' - ' + song.al.name 
-        bam.coverImgUrl = song.al.picUrl
-        //设置全局的播放歌曲的songid
-        app.playingSongId = song.id
-        this.setData({
-          song,
-          playing: true
-        })
-      })
     }
   },
   observers: {
@@ -184,6 +203,8 @@ Component({
           return
         }
         this.checkAndRequestSong(song)
+        this.setPlayNext(song, this.properties.playlist)
+        this.setPlayPrev(song, this.properties.playlist)
       }
     }
   }
